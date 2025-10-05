55120ff66de52350f1c2297056c36ba816c92b97
net-bob <jacklang0909@gmail.com>
2025-10-05 16:07:22 -0400

    My head hurts way too much
    Stupid fucking polygon says my rectangle with length 40 and width 20 has an area of 0
    
    im gonna crash out gng

--- SpaceEvaders/src/game/SpaceEvaders.java
+++ SpaceEvaders/src/game/SpaceEvaders.java
@@ -30,12 +30,24 @@ class SpaceEvaders extends Game {
 		brush.setColor(Color.white);
 //		brush.drawString("Counter is " + counter,10,10);
 		
+		Point a = new Point(0, 0);
+		Point b = new Point(0, 20);
+		Point c = new Point(40, 0);
+		Point d = new Point(40, 20);
+		Point[] points = {a, b, c, d};
+		
+		Polygon test = new Polygon(points, new Point(100, 100), 0);
+		for (int i = 0; i < test.getPoints().length; i++) {
+			System.out.println(test.getPoints()[i]);
+		}
+		
+		
 		/*
 		 * the comment below is just me messing with the code before we meet
 		 * officially on friday
 		 * feel free to delete it or something it doesn't do anything important
 		 */
-		brush.drawString("Time: " + Integer.toString(counter / 60) + ":" + Integer.toString(counter % 60), 10, 10);
+		brush.drawString("Time: " + Integer.toString(counter / 60) + ":" + Integer.toString(counter % 60 * 5 / 3), 10, 10);
 	}
 	public static void main (String[] args) {
 		SpaceEvaders a = new SpaceEvaders();
