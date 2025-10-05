744a1126ad23f3b94e9a64162b2ccd2a43b33eea
net-bob <jacklang0909@gmail.com>
2025-10-05 15:48:41 -0400

    Added shell files for each class

---
+++ SpaceEvaders/src/game/Asteroid.java
@@ -0,0 +1,5 @@
+package game;
+
+public class Asteroid extends Polygon implements Projectile {
+	
+}
---
+++ SpaceEvaders/src/game/Spaceship.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Spaceship extends Polygon {
+	
+	
+	
+	private class Missile extends Polygon implements Projectile {
+		
+	}
+	private class Mine extends Polygon implements Projectile {
+		
+	}
+}
